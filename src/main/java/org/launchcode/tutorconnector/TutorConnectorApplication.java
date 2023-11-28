880ec261d7b55cd8fe89470ae0f8ebcd4f143d42
lorikovarik80 <kovarik.lori@gmail.com>
2023-11-27 20:43:53 -0600

    initial commit

---
+++ src/main/java/org/launchcode/tutorconnector/TutorConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.tutorconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/tutorconnector/TutorConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.tutorconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
