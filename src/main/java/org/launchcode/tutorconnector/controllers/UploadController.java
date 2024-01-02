d46372faa9cf5b72b8da124c0a9a314512037309
VInitha <vinivinitha@gmail.com>
2024-01-02 16:14:01 -0600

    Added upload image files

---
+++ src/main/java/org/launchcode/tutorconnector/controllers/UploadController.java
@@ -0,0 +1,38 @@
+package org.launchcode.tutorconnector.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static jdk.jpackage.internal.WixAppImageFragmentBuilder.Component.File;
+
+@Controller
+public class UploadController {
+
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
+
+
+    @GetMapping("/uploadimage")
+    public String displayUploadForm(){
+        return "imageupload/index";
+
+    }
+
+    @PostMapping("/upload")
+    public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameandPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameandPath,file.getBytes());
+        model.addAttribute("msg", "Uploaded Image" + fileNames.toString());
+        return "imageupload/index";
+    }
+}
