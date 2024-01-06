4e7de4a3c36b155aef6232a04b1763a80ccc6aa7
VInitha <vinivinitha@gmail.com>
2024-01-06 17:02:33 -0600

    Deleted calendar

--- src/main/java/org/launchcode/tutorconnector/controllers/UploadController.java
+++ src/main/java/org/launchcode/tutorconnector/controllers/UploadController.java
@@ -1,5 +1,8 @@
 package org.launchcode.tutorconnector.controllers;
 
+import org.launchcode.tutorconnector.models.data.StudentRepository;
+import org.launchcode.tutorconnector.models.data.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +18,18 @@ import java.nio.file.Paths;
 @Controller
 public class UploadController {
 
+    @Autowired
+    TutorRepository tutorRepository;
+
+    @Autowired
+    StudentRepository studentRepository;
+
+
+
     public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
 
 
-    @GetMapping("/uploadimage")
+    @GetMapping("/upload")
     public String displayUploadForm(){
         return "imageupload/index";
 
