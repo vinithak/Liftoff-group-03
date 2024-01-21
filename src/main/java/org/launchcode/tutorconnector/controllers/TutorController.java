85898510b16baf2ebeb96833cb5805025079f654
VInitha <vinivinitha@gmail.com>
2024-01-20 20:25:19 -0600

    Upgraded reviews and calendar

--- src/main/java/org/launchcode/tutorconnector/controllers/TutorController.java
+++ src/main/java/org/launchcode/tutorconnector/controllers/TutorController.java
@@ -8,6 +8,7 @@ import org.launchcode.tutorconnector.models.data.StudentRepository;
 import org.launchcode.tutorconnector.models.data.SubjectRepository;
 import org.launchcode.tutorconnector.models.data.TutorRepository;
 import org.launchcode.tutorconnector.models.data.TutorReviewRepository;
+import org.launchcode.tutorconnector.models.dto.EditFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,6 +33,9 @@ public class TutorController {
     @Autowired
     private SubjectRepository subjectRepository;
 
+    @Autowired
+    private LoginController loginController;
+
     @RequestMapping("/tutor")
     public String index(Model model) {
 
@@ -42,13 +46,28 @@ public class TutorController {
 
 
     @GetMapping("tutor/profile/{id}")
-    public String displayTutorProfile(@PathVariable int id, Model model) {
+    public String displayTutorProfile(@PathVariable int id, Model model, HttpSession session) {
+       Integer loggedInTutorId = (Integer) session.getAttribute("tutor");
+       Integer loggedInStudentId = (Integer) session.getAttribute("student");
+
         Optional<Tutor> optTutor = tutorRepository.findById(id);
         if (optTutor.isPresent()) {
             Tutor tutor = (Tutor) optTutor.get();
             model.addAttribute("tutor", tutor);
             model.addAttribute("tutorId", tutor.getId());
-            model.addAttribute("students", studentRepository.findAll());
+            if (loggedInStudentId != null) {
+                Optional<Student> optStudent = studentRepository.findById(loggedInStudentId);
+                if(optStudent.isPresent())
+                {
+                    Student student  = (Student) optStudent.get();
+                    model.addAttribute("student", student);
+                }
+                else {
+                    return "redirect:/register";
+                }
+            } else {
+                model.addAttribute("students", studentRepository.findAll());
+            }
             model.addAttribute("subjects", tutor.getSubjects());
             return "tutor/profile";
         } else {
